51776ab336cd53b3110fb37cad3e381169786c4b
UPENDRA VERMA <76901242+upendra-verma19@users.noreply.github.com>
2022-11-17 01:25:00 +0530

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,11 @@
+# more info here:
+# https://www.cyberciti.biz/faq/run-execute-sh-shell-script/
+
+# Set execute permission on your script:
+chmod +x script-name-here.sh
+# To run your script, enter:
+./script-name-here.sh
+# OR
+sh script-name-here.sh
+# OR
+bash script-name-here.sh
